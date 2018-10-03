2d9dca78fe6e2cb4c909f3ad6a8034c770c4272d
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-10-03 16:00:07 +0530

    Add files via upload

---
+++ views/templates/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ciBlog</title>
+  <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">
+
+	</head>
+	<body>
+	 <nav class="navbar navbar-inverse">
+	   <div class="container">
+	   <div class="navbar-header">
+			<a class="navbar-brand" href="/">ciBlog</a>
+
+		</div>
+		<div id="navbar">
+		 <ul class="nav navbar-nav">
+			<li><a href="<?php echo base_url();?>">Home</a></li>
+			<li><a href="<?php echo base_url();?>about">About</a></li>
+		 </ul>
+		</div>
+		
+	   </div>
+	
+
+
+
+</nav>	
+
+<div class="container">
+
