4d44d1baed1821a7aa0a89c9aa0ff7cef29580cb
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-10-05 20:05:34 +0530

    Add files via upload

---
+++ Blog/views/templates/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ciBlog</title>
+  <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
+
+
+	</head>
+	<body>
+	 <nav class="navbar navbar-inverse">``
+	   <div class="container">
+	   <div class="navbar-header">
+			<a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+
+		</div>
+		<div id="navbar">
+		 <ul class="nav navbar-nav">
+			<a href="<?php echo base_url() ?>home">Home</a>
+			<a href="<?php echo base_url() ?>about">About</a>
+			<a href="<?php echo base_url() ?>posts">Blog</a>
+		 </ul>
+		</div>
+		
+	   </div>
+	
+
+
+
+     </nav>	
+     
+
+<div class="container">
+
