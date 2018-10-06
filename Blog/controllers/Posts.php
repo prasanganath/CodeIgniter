7c7985adced6f1572df6f54c1dd7cfe056638ca3
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-10-06 12:59:17 +0530

    update

--- Blog/controllers/Posts.php
+++ Blog/controllers/Posts.php
@@ -52,6 +52,33 @@
 			
 
 		}
+
+		public function delete($id){
+			$this->post_model->delete_post($id);
+			redirect('posts');
+		}
+
+		public function edit($slug){
+			$data['post'] = $this->post_model->get_posts($slug);
+			if (empty($data['post'])){
+				show_404();
+
+				# code...
+			}
+			$data['title'] = 'Edit Post';
+
+			$this->load->view('templates/header');
+			$this->load->view('posts/edit', $data);
+			$this->load->view('templates/footer');
+
+
+		}
+
+		public function update(){
+			$this->post_model->update_post();
+			redirect('posts');
+
+		}   
 	}
 
 
