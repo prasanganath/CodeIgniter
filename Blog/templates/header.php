9777c7723406ded775a64903e0c8546e1159de5e
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-10-07 10:06:45 +0530

    update

--- Blog/templates/header.php
+++ Blog/templates/header.php
@@ -2,8 +2,9 @@
 <html>
 	<head>
 		<title>Blog</title>
-		<link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
-        <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
+		<link rel="stylesheet" href="https://bootswatch.com/4/spacelab/bootstrap.min.css">
+		
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
         <script src="http://cdn.ckeditor.com/4.10.1/standard/ckeditor.js"></script>
 
   
@@ -12,17 +13,21 @@
 	<body>
 	 <nav class="navbar navbar-inverse">
 
+
 	   <div class="container">
+
 
 	   <div class="navbar-header">
 			<a class="navbar-brand" href="<?php echo base_url(); ?>">Blog</a>
 
+
 	   </div>
 		<div id="navbar">
 		 <ul class="nav navbar-nav">
+
 			<li><a href="<?php echo base_url() ?>">Home</a></li>
 			<li><a href="<?php echo base_url() ?>about">About</a></li>
-			<li><a href="<?php echo base_url() ?>posts">Blog</a></li>
+			<li><a href="<?php echo base_url() ?>posts">Posts</a></li>
 		 </ul>
 		 <ul class="nav navbar-nav navbar-right">
 		 	<li><a href="<?php echo base_url() ?>posts/create">Create</a></li>
