894c202215eab0cd1d522800fc289c96dc2f27a2
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-10-08 18:02:38 +0530

    update all

--- Blog/views/templates/header.php
+++ Blog/views/templates/header.php
@@ -2,10 +2,11 @@
 <html>
 	<head>
 		<title>Blog</title>
-		<link rel="stylesheet" href="https://bootswatch.com/4/spacelab/bootstrap.min.css">
+		<link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.min.css">
 		
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
         <script src="http://cdn.ckeditor.com/4.10.1/standard/ckeditor.js"></script>
+        
 
   
 
@@ -32,8 +33,19 @@
 
 		 </ul>
 		 <ul class="nav navbar-nav navbar-right">
-		 	<li><a href="<?php echo base_url() ?>posts/create">Create</a></li>
-		 	<li><a href="<?php echo base_url() ?>categories/create">Create Categories</a></li>
+		 <?php if(!$this->session->userdata('logged_in')) : ?>
+
+			 	<li><a href="<?php echo base_url() ?>users/login">Login</a></li>
+			 	<li><a href="<?php echo base_url() ?>users/register">Register</a></li>
+		 <?php endif; ?>
+		 <?php if($this->session->userdata('logged_in')) : ?>
+
+
+			 	<li><a href="<?php echo base_url() ?>posts/create">Create Post</a></li>
+			 	<li><a href="<?php echo base_url() ?>categories/create">Create Categories</a></li>
+			 	<li><a href="<?php echo base_url() ?>users/logout">Logout</a></li>
+		 <?php endif; ?>	
+			 	
 		 	
 		 </ul>
 		</div>
@@ -49,4 +61,35 @@
      
 
 <div class="container">
+	<!--Flash message-->
+
+	<?php if($this->session->flashdata('user_registered')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_registered').'</p>'; ?>
+	<?php endif; ?>
+	<?php if($this->session->flashdata('post_created')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_created').'</p>'; ?>
+	<?php endif; ?>
+	<?php if($this->session->flashdata('post_updated')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_updated').'</p>'; ?>
+	<?php endif; ?>
+	<?php if($this->session->flashdata('category_created')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('category_created').'</p>'; ?>
+	<?php endif; ?>
+	<?php if($this->session->flashdata('post_deleted')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_deleted').'</p>'; ?>
+	<?php endif; ?>
+	<?php if($this->session->flashdata('login-failed')): ?>
+		<?php echo '<p class="alert alert-danger">'.$this->session->flashdata('login_failed').'</p>'; ?>
+	<?php endif; ?>
+	<?php if($this->session->flashdata('user_loggedin')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_loggedin').'</p>'; ?>
+	<?php endif; ?>
+	<?php if($this->session->flashdata('user_loggedout')): ?>
+		<?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_loggedout').'</p>'; ?>
+	<?php endif; ?>
+	
+
+
+
+
 
